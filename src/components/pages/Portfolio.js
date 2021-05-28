5f44175854ccf26a6587f1d29db753cb99de6399
arrchon <80299255+arrchon@users.noreply.github.com>
2021-05-28 12:20:35 +0900

    200528

---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Portfolio(){
+    return(
+        <div>
+            <h1>Portfolio 페이지</h1>
+        </div>
+    )
+}
+
+export default Portfolio;
---
+++ src/components/pages/Script.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Script(){
+    return(
+        <div>
+            <h1>Script 페이지</h1>
+        </div>
+    )
+}
+
+export default Script;
